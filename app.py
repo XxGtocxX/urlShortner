2bd5b0e198c2a453a33e94c2da416613214804af
XxGtocxX <debanuj.de.official@gmail.com>
2026-05-22 19:37:52 +0530

    fixed issues

--- app.py
+++ app.py
@@ -131,4 +131,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-    app.run(host='0.0.0.0', debug=False)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host='0.0.0.0', port=port)
